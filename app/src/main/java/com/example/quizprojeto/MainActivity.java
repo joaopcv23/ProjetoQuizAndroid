cc93f4a9cacc113f66e6d21c8072b82a75e56c66
joaopcv23 <joaopedrocardosovenancio@gmail.com>
2025-09-30 11:04:19 -0300

    Inicio da parte lógica

--- app/src/main/java/com/example/quizprojeto/MainActivity.java
+++ app/src/main/java/com/example/quizprojeto/MainActivity.java
@@ -3,8 +3,33 @@ package com.example.quizprojeto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txvPergunta;
+
+    RadioGroup rdgPrincipal;
+
+    Button btnResponder;
+
+    int pontuacao = 0;
+
+    int indicePergunta = 0;
+
+    String [] perguntas = {
+            "p1", "p2", "p3, ",
+            "p4", "p5"
+    };
+
+    int[] respostasCorretas= {
+            R.id.rb_a,
+                    R.id.rb_a,
+        R.id.rb_b,
+        R.id.rb_c,
+        R.id.rb_d
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
